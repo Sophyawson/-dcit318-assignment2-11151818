370d749cc0c8cbefe53d10dd3f40e5aa1c069c5f
Sophia <sophieyawson12@gmail.com>
2025-08-09 00:20:06 +0000

    Added the animal sound app that demostrate Inheritance and Method Overriding

---
+++ AnimalSoundApp/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace AnimalSoundApp
+{
+    // Base class called Animal
+    class Animal
+    {
+        public virtual void MakeSound()
+        {
+            Console.WriteLine("Some generic sound");
+        }
+    }
+
+    // Derived class called Dog
+    class Dog : Animal
+    {
+        public override void MakeSound()
+        {
+            Console.WriteLine("Bark");
+        }
+    }
+
+    // Derived class called Cat
+    class Cat : Animal
+    {
+        public override void MakeSound()
+        {
+            Console.WriteLine("Meow");
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Animal genericAnimal = new Animal();
+            Dog dog = new Dog();
+            Cat cat = new Cat();
+
+            genericAnimal.MakeSound(); // Outputs Some generic sound
+            dog.MakeSound();           // Outputs Bark
+            cat.MakeSound();           // Outputs Meow
+        }
+    }
+}
+
