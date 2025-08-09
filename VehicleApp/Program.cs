56b42eb578426f4f6756a6b0cd254e530b76960b
Sophia <sophieyawson12@gmail.com>
2025-08-09 13:45:21 +0000

    added the vehicle app implementing Interfaces

---
+++ VehicleApp/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace MovableApp
+{
+    // Interface definition
+    public interface IMovable
+    {
+        void Move();
+    }
+
+    // Car class implementing IMovable
+    public class Car : IMovable
+    {
+        public void Move()
+        {
+            Console.WriteLine("Car is moving");
+        }
+    }
+
+    // Bicycle class implementing IMovable
+    public class Bicycle : IMovable
+    {
+        public void Move()
+        {
+            Console.WriteLine("Bicycle is moving");
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Create a Car
+            IMovable car = new Car();
+            car.Move();
+
+            // Create a Bicycle
+            IMovable bicycle = new Bicycle();
+            bicycle.Move();
+        }
+    }
+}
+
